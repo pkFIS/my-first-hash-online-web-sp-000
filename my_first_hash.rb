9b266b86053ff694b90f09e41af86f7e6b15028e
Paul K <pkfis@outlook.com>
2019-04-07 22:51:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,7 +24,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  hash["oil paintings"]
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -34,10 +34,10 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-  hash = {"muskets" => 2}
+  shipping_manifest = {"muskets" => 2}
 
   # add 4 gun powder to the shipping_manifest hash below
-  hash = {"gun powder" => 4}
+  shipping_manifest = {"gun powder" => 4}
 
   # return the shipping_manifest hash below
   shipping_manifest
